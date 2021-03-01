cb27a5c5e2ff82b0d41e9d5d3867487d9e979a2f
RonjaMeinecke <ronja.meinecke@gmx.de>
2021-03-01 16:34:52 +0100

    🎉 initial commit set up project

---
+++ src/app.ts
@@ -0,0 +1,7 @@
+const [command] = process.argv.slice(2);
+
+if (command === "set") {
+  console.log("You like to set something?");
+} else if (command === "get") {
+  console.log("What should I get?");
+}
