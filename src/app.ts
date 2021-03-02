f03068e5c6aae2c5143f5442a2a61706212b9178
RonjaMeinecke <ronja.meinecke@gmx.de>
2021-03-02 09:53:42 +0100

    ✨ add questions and chalk

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,33 @@
-const [command] = process.argv.slice(2);
+import prompts from "prompts";
+import chalk from "chalk";
 
-if (command === "set") {
-  console.log("You like to set something?");
-} else if (command === "get") {
-  console.log("What should I get?");
-}
+// console.log(chalk.blue("Hello World!"));
+
+const run = async () => {
+  console.log("Welcome to passwordisland");
+
+  const prompts = require("prompts");
+
+  const questions = [
+    {
+      type: "password",
+      name: "username",
+      message: "enter your username",
+      validate: (username) =>
+        username === "islander" ? true : "you may not enter!",
+    },
+    {
+      type: "password",
+      name: "value",
+      message: "Welcome on the island! Where do you want to go?",
+      validate: (value) =>
+        value === "for a swim" ? true : "you must leave the island!",
+    },
+  ];
+
+  async () => {
+    const response = await prompts(questions);
+  };
+};
+
+run();
