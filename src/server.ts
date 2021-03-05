3d72e8a09b27b7289935224f48d5a50de32e1f43
RonjaMeinecke <ronja.meinecke@gmx.de>
2021-03-05 12:00:40 +0100

    ✨ create Server and connect to localhost

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import http from "http";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT;
+console.log("Port is: ", port);
+
+const server = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader("Content-Type", "application/json");
+  response.end(JSON.stringify([1, 2, 3]));
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:$(port)`);
+});
